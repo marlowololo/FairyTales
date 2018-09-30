6df28ee06fd4fa0611065ae7235db93c5f2cab28
Insan Athoriq <insan.athoriq@gameloft.com>
2018-09-30 19:47:53 +0700

    + Implement character movement and basic camera following

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public GameObject Player;
+
+    private Vector3 CameraInitialPosition;
+
+    void Start() {
+        CameraInitialPosition = transform.position;
+    }
+
+	void Update () {
+        transform.position = Player.transform.position + CameraInitialPosition;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float movementSpeed;
+
+    private Rigidbody rb;
+    private bool moving;
+    private Vector3 rotationVector;
+    private float horizontalInput, verticalInput;
+
+    // Use this for initialization
+    void Awake() {
+        rb = GetComponent<Rigidbody>();
+        moving = false;
+        rotationVector = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        if(Input.GetButton("Horizontal") || Input.GetButton("Vertical")) {
+            rotationVector.Set(horizontalInput, 0, verticalInput);
+            transform.rotation = Quaternion.LookRotation(rotationVector);
+            moving = true;
+        }
+
+        if(Input.GetButtonUp("Horizontal") || Input.GetButtonUp("Vertical")) {
+            moving = false;
+        }
+    }
+
+    void FixedUpdate() {
+        if(moving) {
+            rb.velocity = transform.forward * movementSpeed;
+        }
+    }
+}
